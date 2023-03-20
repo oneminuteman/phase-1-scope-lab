ae3a90bc3d19849d1d0e9f8ab1b1cc3893bd0c76
Victor <victor.ireri@student.moringaschool.com>
2023-03-20 09:03:28 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var customerName = "Victor Ireri";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not kate";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "new best customer";
+}
+
+const leastFavoriteCustomer = "Victor Ireri";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = " Greg Sullivan";
+  
+}
